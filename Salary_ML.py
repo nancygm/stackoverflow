f71c94ae3ea6bdc2b42afd203d142ae91de48be3
Nancy Giron <nancy.giron@bowpi.com>
2021-06-07 21:27:39 -0600

    Last changes

--- Salary_ML.py
+++ Salary_ML.py
@@ -22,7 +22,6 @@ def clean_data(df):
     4. Drop the Salary, Respondent, and the ExpectedSalary columns
     5. For each numeric variable, fill the column with the mean value.
     6. Create dummy columns for all the categorical variables, drop the original columns
-    7.
     '''
     # Drop rows with missing salary values
     df = df.dropna(subset=['Salary'], axis=0)
